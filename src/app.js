5b3d7bb3808e18680f2eed7ee41041397f0bbe60
LoboArkano <pipiolo_mananes@hotmail.com>
2020-09-10 15:47:25 -0500

    Add app file to start the web page

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 import header from './header';
+import { formCont } from './form';
 import userInterface from './user-interface';
 
 const app = () => {
   const content = document.getElementById('content');
 
+  content.appendChild(formCont());
   content.appendChild(header());
   content.appendChild(userInterface());
 };
