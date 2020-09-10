20f7e8f9462acf6bc2a9ca69011cab391649cfc8
LoboArkano <pipiolo_mananes@hotmail.com>
2020-09-10 15:46:01 -0500

    Add functions to create todo object

--- src/controller.js
+++ src/controller.js
@@ -19,4 +19,27 @@ const createProject = (btn) => {
   });
 };
 
-export { createProject };
+const createTodo = (btn) => {
+  btn.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    const formCont = document.getElementById('project-form-cont');
+    const title = document.getElementById('todo-title');
+    const dueDate = document.getElementById('todo-dueDate');
+    const priority = document.getElementById('todo-priority');
+    const description = document.getElementById('todo-desc');
+    const note = document.getElementById('todo-note');
+    const newTodo = {};
+
+    newTodo.title = title.value;
+    newTodo.dueDate = dueDate.value;
+    newTodo.priority = priority.value;
+    newTodo.description = description.value;
+    newTodo.note = note.value;
+
+    LocalStorageModule.saveTodo(newTodo, 'Project 1');
+    formCont.classList.add('d-none');
+  });
+};
+
+export { createProject, createTodo };
