29b734d2daa980897e7f1bccdba1cd9edbb2c150
LoboArkano <pipiolo_mananes@hotmail.com>
2020-09-10 15:46:43 -0500

    Add functions to add and delete todo

--- src/storage.js
+++ src/storage.js
@@ -20,11 +20,31 @@ const LocalStorageModule = (() => {
     return projects;
   };
 
+  const saveTodo = (todo, key) => {
+    const project = new Map(JSON.parse(localStorage.getItem(key)));
+    const todos = new Map(project.get('todos'));
+
+    todos.set(todo.title, todo);
+    project.set('todos', Array.from(todos));
+    localStorage.setItem(key, JSON.stringify(Array.from(project.entries())));
+  };
+
+  const deleteTodo = (todo, key) => {
+    const project = new Map(JSON.parse(localStorage.getItem(key)));
+    const todos = new Map(project.get('todos'));
+
+    todos.delete(todo.title);
+    project.set('todos', Array.from(todos));
+    localStorage.setItem(key, JSON.stringify(Array.from(project.entries())));
+  };
+
   return {
     saveProject,
     deleteProject,
     getProject,
     getAllProjects,
+    saveTodo,
+    deleteTodo,
   };
 })();
 
